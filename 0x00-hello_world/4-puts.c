4d387519cb3e5e3c60500c7fb262f7a94ddce866
EmediongUmana <emediongchrisumana@gmail.com>
2022-12-08 14:49:41 -0800

    task 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /*
  * main - Entry point
  *Return: Always 0   
@@ -10,7 +8,6 @@
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle"\n);
-	    return (0);
+	puts("Programming is like building a multilingual puzzle"\n);			   return (0);
 
 }
