33c8962e7eb252e37a223d65bcee0a66ed3ec8eb
EmediongUmana <emediongchrisumana@gmail.com>
2022-12-08 15:56:05 -0800

    Hello, puts@ git commit -m @Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,9 @@
-#include <stdio.h>                                                                                                                      
-
-/*                                                                                                                                     
- *
- *   main - Entry                                                                                                                      
- *   Return: Always 0
+#include <stdio.h>                                                                                                     
+/**
+ *main - Entry                 
+ *Return: Always 0 (Success)
  */                                                                                                                                     
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
-
-			        return (0);
-
+int main(void)
+{                                
+      puts("\"Programming is like building a multi lingual puzzle");                                      return (0);
 }
