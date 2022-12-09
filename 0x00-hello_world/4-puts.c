ab38e4ec8f423cfed8f241fcd83b8b33233020f0
EmediongUmana <emediongchrisumana@gmail.com>
2022-12-08 16:04:18 -0800

    @helloput@

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>                                                                                                     
 /**
- *main - Entry                 
+ *main - Entry
  *Return: Always 0 (Success)
- */                                                                                                                                     
+ */
+
 int main(void)
-{                                
-      puts("\"Programming is like building a multi lingual puzzle");                                      return (0);
+{
+	puts("n\Programming is like building a multi lingual puzzle");
+ 	 return (0);
 }
