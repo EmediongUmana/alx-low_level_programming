09938f15af76aa8d130e0a3091bd9c74582c63b4
EmediongUmana <emediongchrisumana@gmail.com>
2022-12-09 14:35:47 -0800

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - main function to generate a random number
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  *
  */
 /* betty style doc for function main goes there */
@@ -16,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
