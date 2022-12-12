bde091991476ce4d7870a07fd605686f96b2ce11
EmediongUmana <emediongchrisumana@gmail.com>
2022-12-12 12:25:58 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,11 @@
  *
  */
 int main(void)
-{	
+{
 	int a;
-	for (a =0; a <10; a++)
-			printf("%d", a);
-		putchar('\n');
-		return(0);
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
 }
